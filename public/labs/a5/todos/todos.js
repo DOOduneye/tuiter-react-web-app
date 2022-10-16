69539422a2b2f276768668b46f024e0a10dda4ae
David Oduenye <davidoduneye1@gmail.com>
2022-10-16 18:43:49 -0400

    a5 javascript fa22

---
+++ public/labs/a5/todos/todos.js
@@ -0,0 +1,21 @@
+// export default [
+//     "Buy milk",
+//     "Pickup the kids",
+//     "Walk the dog"
+// ]
+
+// Json Object
+export default [
+    {
+        title: "Buy milk",
+        done: true
+    },
+    {
+        title: "Pickup the kids",
+        done: false
+    },
+    {
+        title: "Walk the dog",
+        done: false
+    }
+]
