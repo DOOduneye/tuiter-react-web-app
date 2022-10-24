38a9153cbe7c8e7b7cb53cea62d334c2d3c1d302
David Oduenye <davidoduneye1@gmail.com>
2022-10-23 23:17:57 -0400

    a6

--- src/tuiter/navigation-sidebar/NavigationSidebar.js
+++ src/tuiter/navigation-sidebar/NavigationSidebar.js
@@ -4,60 +4,69 @@ const NavigationSidebar = ({active = 'explore'}) => {
     return (
         <div>
             <div className="list-group list-group-flush border rounded-3">
-                <a className="list-group-item" href="/">
+                <a className={`list-group-item
+                    ${active === 'tuiter' ? 'active' : ''}`} href="home.html">
                     Tuiter
                 </a>
 
-                <a className="list-group-item" href="../home/home.html">
+                <a className={`list-group-item
+                    ${active === 'home' ? 'active' : ''}`} href="/tuiter/">
                     <i className="fa-solid fa-home"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Home
                     </span>
                 </a>
 
-                <a className="list-group-item bg-primary text-white" href="../ExploreScreen/explore.html">
+                <a className={`list-group-item
+                    ${active === 'explore'?'active':''}`} href="/tuiter/">
                     <i className="fa-solid fa-hashtag"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Explore
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../notifications/notifications.html">
+                <a className={`list-group-item
+                    ${active === 'notifications'?'active':''}`} href="/notifications">
                     <i className="fa-solid fa-bell"></i> 
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Notifications
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../messages/messages.html">
+                <a className={`list-group-item
+                    ${active === 'messages'?'active':''}`} href="/messages">
                     <i className="fa-solid fa-envelope"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Messages
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../bookmarks/bookmarks.html">
+                <a className={`list-group-item
+                    ${active === 'bookmarks'?'active':''}`} href="/bookmarks">
                     <i className="fa-solid fa-bookmark"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Bookmarks
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../lists/lists.html">
+                <a className={`list-group-item
+                    ${active === 'lists'?'active':''}`} href="/lists">
                     <i className="fa-solid fa-list"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Lists
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../profile/profile.html">
+                <a className={`list-group-item
+                    ${active === 'profile'?'active':''}`} href="/profile">
                     <i className="fa-solid fa-user"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         Profile
                     </span>
                 </a>
 
-                <a className="list-group-item" href="../more/more.html">
+                <a className={`list-group-item
+                    ${active === 'more'?'active':''}`} href="/more">
                     <i className="fa-solid fa-ellipsis-h"></i>
                     <span className="p-1 d-none d-xl-inline d-xxl-inline">
                         More
