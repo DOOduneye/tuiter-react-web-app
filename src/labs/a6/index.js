0ede0d5dee8aa27a6dc650a9bf515d2b246e2a9d
David Oduenye <davidoduneye1@gmail.com>
2022-11-05 17:05:34 -0400

    update

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,12 +2,10 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo/todo-list";
-import Nav from "../../nav";
 
 function Assignment6() {
  return (
    <div>
-    <Nav />
     <h1>Assignment 6</h1>
     <TodoList />
     <ConditionalOutput />
