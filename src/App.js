f0b6edeb7c45c354bf60cc7ace9f4f680992f250
David Oduneye <davidoduneye@Davids-MacBook-Air.local>
2022-11-06 22:30:26 -0500

    A7

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ const App = () => {
           <div className="container">
               <Routes>
                   <Route index element={<Labs/>}/>
-                  <Route path="/*" element={<Labs/>}/>
                   <Route path="/hello" element={<HelloWorld/>}/>
                   <Route path="/explore" element={<Tuiter />}/>
                   <Route path="/tuiter" element={<Home />}/>
