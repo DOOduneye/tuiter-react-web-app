a991729917930b16c5e71f88c8356547c97784c7
David Oduneye <davidoduneye@Davids-MacBook-Air.local>
2022-11-06 22:25:18 -0500

    revert

--- public/labs/a5/todos/index.js
+++
@@ -1,8 +0,0 @@
-import TodoList from "./TodoList.js";
-
-$('#wd-todo').append(`
-    <div class="container">
-        <h1>Todo Example</h1>
-        ${TodoList()}
-    </div>
-`);
--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
+import React from 'react';
 import './App.css';
-import Labs from './labs/index.js';
-import HelloWorld from "./labs/a6/hello-world";
-import Tuiter from "./tuiter/Tuiter";
-import Home from "./tuiter/Home";
-
-import {BrowserRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
+import './index.css'
+import logo from './logo.svg';
 
 const App = () => {
   return (
-<<<<<<< HEAD
-      <BrowserRouter>
-          <div className="container">
-              <Routes>
-                  <Route index element={<Labs/>}/>
-                  <Route path="/*" element={<Labs/>}/>
-                  <Route path="/hello" element={<HelloWorld/>}/>
-                  <Route path="/explore" element={<Tuiter />}/>
-                  <Route path="/tuiter" element={<Home />}/>
-              </Routes>
-          </div>
-      </BrowserRouter>
-=======
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            Welcome to Web Development Fall 2022  
+            </p>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+            </a>
+        </header>  
     </div>
->>>>>>> parent of 30661f0 (edited paragraph)
   );
 }
 
