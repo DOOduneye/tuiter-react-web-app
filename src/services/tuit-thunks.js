1ff3026d170ed1192b04b5294a0a394ae7a0abe0
David Oduenye <davidoduneye1@gmail.com>
2022-11-17 13:58:01 -0500

    merged

--- src/services/tuit-thunks.js
+++
@@ -1,4 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import * as service from './tuit-services';
-
-export const findTuitsThunk = createAsyncThunk('tuits/findTuits', async () => await service.findTuits());
